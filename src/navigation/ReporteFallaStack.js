76cecccd8d01bd02a3ede13ab1cb49afd53bda28
hhuezo <hugo.alex.huezo@gmail.com>
2023-12-16 08:11:38 -0600

    index de reporte de falla

--- src/navigation/ReporteFallaStack.js
+++ src/navigation/ReporteFallaStack.js
@@ -1,13 +1,20 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ReporteFallaScreen } from "../screens/ReporteFalla/ReporteFallaScreen";
 import { ReporteMapaScreen } from "../screens/ReporteFalla/ReporteMapaScreen";
+import { ReporteIndexScreen } from "../screens/ReporteFalla/ReporteIndexScreen";
 import React from "react";
+
 
 const Stack = createNativeStackNavigator();
 
 export function ReporteFallaStack() {
   return (
     <Stack.Navigator>
+       <Stack.Screen
+        name="ReporteIndexStack"
+        component={ReporteIndexScreen}
+        options={{ title: "Reporte de falla" }}
+      />
       <Stack.Screen
         name="ReporteMapaStack"
         component={ReporteMapaScreen}
