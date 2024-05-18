b39e73f6746407efcbc9476d8675a1bc105c6881
hhuezo <hugo.alex.huezo@gmail.com>
2024-05-18 10:09:12 -0600

    url api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-//export const API_HOST = "https://dgehm.corpweb.info/public/api";
-export const API_HOST = "http://192.168.1.14:8000/api";
+export const API_HOST = "https://dgehm.corpweb.info/public/api";
+//export const API_HOST = "http://192.168.1.14:8000/api";
 
